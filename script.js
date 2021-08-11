067a71b3956b55f37899fcd2022fbf1b5d2538be
PinkSpaceBee <minaary@mailfence.com>
2021-08-11 17:40:49 +0300

    create a 16x16 grid

---
+++ script.js
@@ -0,0 +1,12 @@
+let container = document.querySelector('.container');
+let gridCell;
+
+for(let i = 0; i < 256; i++) {
+    gridCell = document.createElement('div')
+    container.appendChild(gridCell);
+}
+
+
+
+
+
