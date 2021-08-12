d32138d832a8cb427877bab0990df54708569d5e
PinkSpaceBee <minaary@mailfence.com>
2021-08-12 18:17:20 +0300

    add range slider, css is fucking weird

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let container = document.querySelector('.js-container');
 let gridCell;
 let resetButton = document.querySelector('#js-reset-button');
 let cells;
+let gridSize = document.querySelector('#js-slider');
+let sliderValue = document.querySelector('#js-slider-value');
 
 function createGrid() {
     for(let i = 0; i < 256; i++) {
@@ -27,7 +29,15 @@ resetButton.addEventListener('click', () => {
     removeGrid();
     createGrid();
 })
+    
+let value = document.querySelector('#js-slider-value');
 
+gridSize.addEventListener('change', () => {
+    let count = gridSize.value;
+
+    value.textContent = `${count}x${count}`;
+    gridSize.textContent = count;
+})
 
 
 
