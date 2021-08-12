7701962c8614321afdcb25269998d9e47e0eeed5
PinkSpaceBee <minaary@mailfence.com>
2021-08-12 13:14:44 +0300

    add hover effect

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let container = document.querySelector('.container');
 let gridCell;
 
 for(let i = 0; i < 256; i++) {
-    gridCell = document.createElement('div')
+    gridCell = document.createElement('div');
     container.appendChild(gridCell);
 }
 
+//let cells = container.children;
+let cells = Array.from(container.children);
 
+cells.forEach(cell => cell.addEventListener('mouseover', () => {
+    cell.style.background = 'rgba(255, 135, 135, 1)';
+}))
 
 
